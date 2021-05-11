524edbb444108c84711de0e0c3a74e3596212e4b
korzio-box <korneliusz.ziolkowski@gmail.com>
2021-05-11 22:57:41 +0200

    mini change in events model - added second date and deleted an problem with a status field

--- events/models.py
+++ events/models.py
@@ -6,14 +6,15 @@ from customers.models import Client
 
 class Event(models.Model):
     id = models.AutoField(primary_key=True, blank=False, null=False)
+    status = models.CharField(max_length=1, choices=EStatus.choices, null=False, blank=False)
+    client = models.ForeignKey(Client, null=True, blank=False, on_delete=models.SET_NULL)
+    time_add = models.DateTimeField(auto_now=False, auto_now_add=False, blank=False, null=False)
+    time_done = models.DateTimeField(auto_now=False, auto_now_add=False, blank=False, null=False)
+    Product = models.ManyToManyField(Product, blank=True)
+    Service = models.ManyToManyField(Service, blank=True)
     class EStatus(models.TextChoices):
         planned = '1', "Planowane" 
         done = '2', "Zatwierdzone"
-    status = models.CharField(max_length=1, choices=EStatus.choices, null=False, blank=False)
-    client = models.ForeignKey(Client, null=True, blank=False, on_delete=models.SET_NULL)
-    timestamp = models.DateTimeField(auto_now=False, auto_now_add=False, blank=False, null=False)
-    Product = models.ManyToManyField(Product, blank=True)
-    Service = models.ManyToManyField(Service, blank=True)
 
     def __str__(self):
         return str(self.timestamp)[0:10] + "/" + str(self.timestamp)[11:16] + "/" + str(self.id)
