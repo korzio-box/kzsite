1b00af567a86d98bb17725c714501c811b3e9627
Korzi0 <korneliusz.ziolkowski@gmail.com>
2021-07-06 15:24:04 +0200

    aaad

--- warehouse/views.py
+++ warehouse/views.py
@@ -25,3 +25,34 @@ class ProductEditView(UpdateView):
     form_class = ProductEditForm
     template_name = 'warehouse/product_edit.html'
 
+class PTypeListView(ListView):
+    model = PType
+    context_object_name = 'ptype_list'
+    template_name = 'warehouse/ptype_list.html'
+
+class PGroupListView(ListView):
+    model = PGroup
+    context_object_name = 'pgroup_list'
+    template_name = 'warehouse/pgroup_list.html'
+
+class PTypeDetailView(DetailView):
+    model = PType
+    context_object_name = 'ptype_detail'
+    template_name = 'warehouse/ptype_detail.html'
+
+class PGrouptDetailView(DetailView):
+    model = PGroup
+    context_object_name = 'pgroup_detail'
+    template_name = 'warehouse/pgroup_detail.html'
+
+class PTypeCreateView(CreateView):
+    model = PType
+    context_object_name = 'ptype_create'
+    form_class = ProductCreateForm
+    template_name = 'warehouse/ptype_create.html'
+
+class PGrouproductCreateView(CreateView):
+    model = PGroup
+    context_object_name = 'pgroup_create'
+    form_class = ProductCreateForm
+    template_name = 'warehouse/product_create.html'
