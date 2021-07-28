ad265a01169f37973f92d96c1b7e2022405a4cb0
Korzi0 <korneliusz.ziolkowski@gmail.com>
2021-07-28 14:31:58 +0200

    gggg

--- events/models.py
+++ events/models.py
@@ -5,16 +5,16 @@ from customers.models import Client
 # Create your models here.
 
 class Event(models.Model):
-    class EStatus(models.TextChoices):
-        planned = '1', "Planowane" 
-        done = '2', "Zatwierdzone"
     id = models.AutoField(primary_key=True, blank=False, null=False)
-    status = models.CharField(max_length=1, choices=EStatus.choices, null=False, blank=False)
     client = models.ForeignKey(Client, null=True, blank=False, on_delete=models.SET_NULL)
     time_add = models.DateTimeField(auto_now=False, auto_now_add=False, blank=False, null=False)
     time_done = models.DateTimeField(auto_now=False, auto_now_add=False, blank=False, null=False)
     Product = models.ManyToManyField(Product, blank=True)
+    CHOICES_STATUS = (
+        (1, "Planowane"),
+        (2, "Wykonane"))
 
+    status = models.PositiveSmallIntegerField(choices=CHOICES_STATUS, blank=True)
 
     def __str__(self):
         return str(self.time_done)[0:10] + "/" + str(self.time_done)[11:16] + "/" + str(self.id)
